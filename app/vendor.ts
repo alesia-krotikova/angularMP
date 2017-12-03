2153f1d525c510b631ff792c86b049d9fd2a7b55
alesia-krotikova <alesia.krotikova@gmail.com>
2017-12-03 14:33:26 +0300

    added initial structure (#1)
    * added initial structure

---
+++ app/vendor.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata';
+import 'rxjs/Observable';
+import 'zone.js/dist/zone';
+
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/core';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var webpack = require("webpack"),
+  path = require('path');
+
+const ccpOptions = {
+  name: 'vendor',
+  filename: './dist/vendor.bundle.js'
+};
+
+function root(__path) {
+  return path.join(__dirname, __path);
+}
+
+module.exports = {
+  entry: {
+    "vendor": "./app/vendor",
+    "app": "./app/main"
+  },
+  output: {
+    path: __dirname
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [
+      path.resolve('./app'),
+      'node_modules'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(ccpOptions),
+
+    new webpack.ContextReplacementPlugin(
+      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      root('./src'),
+      { }
+    ),
+
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
+  ]
+};
